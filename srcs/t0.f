db53d2ba352425bc7c7831c4965420f59411f24e
agsb <barcellos.alvaro@gmail.com>
2026-03-30 21:13:14 -0300

    update smalls

--- srcs/t0.f
+++ srcs/t0.f
@@ -1,206 +1,107 @@
-
- : void u@ 0# ;
- see
-
- : | .S .R ;
- see
+ : void ;
 
  : -1 u@ 0# ;
- see
-
- :  0 -1 -1 nand ;
- see
-
+ : 0 -1 -1 nand ;
  : TRUE -1 ;
- see
-
  : FALSE 0 ;
- see
-
- :  1 -1 -1 + -1 nand ;
- see
-
- :  2 1 1 + ;
- see
  
- :  4 2 2 + ;
- see
+ : 1 -1 -1 + -1 nand ;
+ : 2 1 1 + ;
+ : 3 2 1 + ;
+ : 4 2 2 + ;
 
+ : cell 4 ;
+ 
  : sp u@ ;
- see
  : rp sp cell + ;
- see
- : last rp cell + ;
- see
- : here last cell + ;
- see
- : >in here cell + ;
- see
+ : latest rp cell + ;
+ : heap latest cell + ;
+ : >in heap cell + ;
  : state >in cell + ;
- see
-
- : rp@ rp @ cell + ;
- see
  
+ : rp@ rp @ cell + ;
  : sp@ sp @ cell + ;
- see
  
  : dup sp@ @ ;
- see
-
  : over sp@ cell + @ ;
- see
- 
  : swap over over sp@ cell + cell + cell + ! sp@ cell + ! ;
- see
-
  : not dup nand ;
- see
-
  : and nand not ;
- see
-
  : or not swap not and not ;
- see
-
- : - not 1 + + ;
- see
-
- : <> - 0# ;
- see
-
- : = <> not ;
- see
-
- : drop dup - + ;
- see
-
- : nip swap drop ;
- see
-
- : tuck swap over ;
- see
-
- : 2dup over over ;
- see
-
- : 2drop drop drop ;
- see
-
- : allot here @ + here ! ;
- see
  
- : , here @ ! cell allot ;
- see
-
+ : - not 1 + + ;
+ : <> - 0# ;
+ : = <> not ;
+ 
+ : drop dup - + ;
+ : nip swap drop ;
+ : tuck swap over ;
+ : 2dup over over ;
+ : 2drop drop drop ;
+ 
+ : here heap @ ;
+ : allot here + heap ! ;
+ : , here ! cell allot ;
  : >r rp@ @ swap rp@ ! rp@ cell - rp ! rp@ ! ;
- see
-
  : r> rp@ @ rp@ cell + rp ! rp@ @ swap rp@ ! ;
- see
-
+ 
  : branch rp@ @ dup @ + rp@ ! ;
- see
  : ?branch 0# not rp@ @ @ cell - and rp@ @ + cell + rp@ ! ;
- see
  
  : lit rp@ @ dup cell + rp@ ! @ ;
- see
  : ['] rp@ @ dup cell + rp@ ! @ ;
- see
-
- : rot >r swap r> swap ;
- see
 
  : 2* dup + ;
  : 2** 2* 2* 2* 2* 2* 2* 2* 2* ;
  : 80h 1 2* 2* 2* 2* 2* 2* 2* ;
  : IMMEDIATE 80h 2** 2** 2** ;
- see
- 
- : immediate last @ cell + dup @ IMMEDIATE or swap ! ;
- see
+ : immediate latest @ cell + dup @ IMMEDIATE or swap ! ;
  
  : ] 1 state ! ;
- see
  : [ 0 state ! ; immediate
- see
+ : postpone -1 state ! ; immediate
  
- : if ['] ?branch , here @ 0 , ; immediate
- see
- : then dup here @ swap - swap ! ; immediate
- see
- : else ['] branch , here @ 0 , swap dup 
-     here @ swap - swap ! ; immediate
- see
+ : if ['] ?branch , here 0 , ; immediate
+ : then dup here swap - swap ! ; immediate
+ : else ['] branch , here 0 , swap dup 
+   here swap - swap ! ; immediate
  
- : begin here @ ; immediate
- see
- : again ['] branch , here @ - , ; immediate
- see
- : until ['] ?branch , here @ - , ; immediate
- see
- : while ['] ?branch , here @ 0 , ; immediate
- see
- : repeat swap ['] branch , here @ - , 
-     dup here @ swap - swap ! ; immediate
- see
+ : begin here ; immediate
+ : again ['] branch , here - , ; immediate
+ : until ['] ?branch , here - , ; immediate
  
- : do here @ ['] >r , ['] >r , ; immediate
- see
+ : while ['] ?branch , here 0 , ; immediate
+ : repeat swap ['] branch , here - , 
+   dup here swap - swap ! ; immediate
+ 
+ : do here ['] >r , ['] >r , ; immediate
  : loop ['] r> , ['] r> , ['] lit , 1 , ['] + , 
-     ['] 2dup , ['] = , ['] ?branch , 
-     here @ - , ['] 2drop , ; immediate
- see
+   ['] 2dup , ['] = , ['] ?branch , 
+   here - , ['] 2drop , ; immediate
  
- :  8 lit [ 4 4 + , ] ;
- see
- 
+ : 8 lit [ 4 4 + , ] ;
  : 16 lit [ 8 8 + , ] ;
- see
- 
  : bl lit [ 16 16 + , ] ;
- see
-
  : cr lit [ 8 2 + , ] emit ;
- see
- 
  : nl lit [ 8 4 + 1 + , ] emit ;
- see
-
  : CHAR lit [ 16 1 - 2* 2* 2* 2* 16 1 - or , ] ;
- see
-
  : c@ @ CHAR and ;
- see
-
+ 
  : type 0 do dup c@ emit 1 + loop drop ;
- see
- 
- : in> >in @ c@ >in dup @ 1 + swap ! ;
- see
-
- : parse in> drop >in @ swap 0 
-        begin over in> <> while 1 + repeat 
-        swap bl = if >in dup @ 1 - swap ! then ;
- see
-
- : word in> drop begin dup in> <> until >in @ cell - >in ! parse ;
- see
- 
+ : in> >in @ c@ >in @ 1 + >in ! ;
+ : parse in> drop 
+   >in @ swap 0 
+   begin over in> <> while 1 + repeat 
+   swap bl = if >in @ 1 - >in ! then ;
+ : word in> drop 
+   begin dup in> <> until 
+   >in @ 2 - >in ! parse ;
  : [char] ['] lit , bl word drop c@ , ; immediate
- see
-
+ 
  : ( [char] ) parse drop drop ; immediate
- see
- 
  : ." [char] " parse type ; immediate
- see
- 
+
  ." Hello world " cr
  
  ." That's all Folks !" cr
- 
- words 
 
- dump
